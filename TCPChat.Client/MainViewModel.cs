fa08981eda88c96d6a005bc1a664002e72cc0e69
Nicu <cabac2001@gmail.com>
2023-03-21 15:29:41 +0200

    TCPChat

--- TCPChat.Client/MainViewModel.cs
+++ TCPChat.Client/MainViewModel.cs
@@ -1,14 +1,9 @@
 ﻿using DevExpress.Mvvm;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Net;
 using System.Net.Sockets;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Input;
 
 namespace TCPChat.Client
 {
